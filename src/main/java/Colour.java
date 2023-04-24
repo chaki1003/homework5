d0b777e9c3f62cb4a9e2cdc2542f90a2a3fa7bc4
chaki1003 <jihongchamu439@gmail.com>
2023-04-24 15:41:08 +0900

    ファイル末尾を改行

--- src/main/java/Colour.java
+++ src/main/java/Colour.java
@@ -4,6 +4,7 @@ public class Colour{
 
     public static void main(String[] args) {
         List<String> names = List.of("red", "blue", "yellow", "purple", "black");
+
         names.stream().map(String::toUpperCase).forEach(System.out::println);
     }
 }
